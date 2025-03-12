7e937e0f4f63e80838d4bd641ceec705e31da062
Daqi Song <dqaria@gmail.com>
2025-03-12 16:57:32 +1030

    feat: add bin merge scripts

--- extra_script.py
+++ extra_script.py
@@ -2,8 +2,11 @@ Import('env')
 import os
 import shutil
 
+# TODO: add merge
+# https://github.com/eurofurence/ef28-badge/blob/main/merge-bin.py
+
 OUTPUT_DIR = "build_output{}".format(os.path.sep)
-#OUTPUT_DIR = os.path.join("build_output")
+APP_BIN = "$BUILD_DIR/${PROGNAME}.bin"
 
 def _get_cpp_define_value(env, define):
     define_list = [item[-1] for item in env["CPPDEFINES"] if item[0] == define]
@@ -17,12 +20,34 @@ def _create_dirs(dirs=["map", "release", "firmware"]):
     for d in dirs:
         os.makedirs(os.path.join(OUTPUT_DIR, d), exist_ok=True)
 
+def copy_merge_bins(firmware_src, env):
+    flash_images = env.Flatten(env.get("FLASH_EXTRA_IMAGES", [])) + ["$ESP32_APP_OFFSET", APP_BIN]
+    name = firmware_src.split(os.path.sep)[2]
+    flash_size = env.GetProjectOption("board_upload.flash_size")
+    board = env.BoardConfig()
+    f_flash = board.get("build.f_flash", "40000000L")
+    flash_freq = '40m'
+    if (f_flash == '80000000L'):
+        flash_freq = '80m'
+    mcu = board.get("build.mcu", "esp32")
+    firmware_dst = "{}{}_{}_{}_0x0.bin".format(OUTPUT_DIR, mcu, name, flash_size)
+    if os.path.isfile(firmware_dst):
+        os.remove(firmware_dst)
+    cmd = " ".join(
+        ["$PYTHONEXE", "$OBJCOPY", '--chip', mcu, 'merge_bin', '--output', firmware_dst, '--flash_mode', 'dio',
+         '--flash_size', flash_size, '--flash_freq', flash_freq] + flash_images)
+    env.Execute(cmd)
+    return firmware_dst
+
 def create_release(source):
     release_name = _get_cpp_define_value(env, "RELEASE_NAME")
     version = _get_cpp_define_value(env, "EGLANCE_VERSION")
+
+    firmware_dst = copy_merge_bins(source, env)
+
     release_file = os.path.join(OUTPUT_DIR, "release", f"eglance_{version}_{release_name}.bin")
-    print(f"Copying {source} to {release_file}")
-    shutil.copy(source, release_file)
+    print(f"Copying {firmware_dst} to {release_file}")
+    shutil.copy(firmware_dst, release_file)
 
 def bin_rename_copy(source, target, env):
     _create_dirs()
