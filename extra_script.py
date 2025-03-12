b814ce60b3375b5a37590fcc3b230f2670544756
Daqi Song <dqaria@gmail.com>
2025-03-12 14:05:30 +1030

    feat: change release code

--- extra_script.py
+++ extra_script.py
@@ -1,11 +1,46 @@
-Import("env")
+Import('env')
+import os
+import shutil
 
-# Access to global construction environment
-build_tag = env['PIOENV']
+OUTPUT_DIR = "build_output{}".format(os.path.sep)
+#OUTPUT_DIR = os.path.join("build_output")
 
-# Dump construction environment (for debug purpose)
-# print(env.Dump())
+def _get_cpp_define_value(env, define):
+    define_list = [item[-1] for item in env["CPPDEFINES"] if item[0] == define]
 
-# Rename binary according to environnement/board
-# ex: firmware_esp32dev.bin or firmware_nodemcuv2.bin
-env.Replace(PROGNAME="firmware_%s" % build_tag)
+    if define_list:
+        return define_list[0]
+
+    return None
+
+def _create_dirs(dirs=["map", "release", "firmware"]):
+    for d in dirs:
+        os.makedirs(os.path.join(OUTPUT_DIR, d), exist_ok=True)
+
+def create_release(source):
+    release_name = _get_cpp_define_value(env, "RELEASE_NAME")
+    version = _get_cpp_define_value(env, "EGLANCE_VERSION")
+    release_file = os.path.join(OUTPUT_DIR, "release", f"eglance_{version}_{release_name}.bin")
+    print(f"Copying {source} to {release_file}")
+    shutil.copy(source, release_file)
+
+def bin_rename_copy(source, target, env):
+    _create_dirs()
+    variant = env["PIOENV"]
+    builddir = os.path.join(env["PROJECT_BUILD_DIR"],  variant)
+    source_map = os.path.join(builddir, env["PROGNAME"] + ".map")
+
+    # create string with location and file names based on variant
+    map_file = "{}map{}{}.map".format(OUTPUT_DIR, os.path.sep, variant)
+
+    create_release(str(target[0]))
+
+    # copy firmware.map to map/<variant>.map
+    if os.path.isfile("firmware.map"):
+        print("Found linker mapfile firmware.map")
+        shutil.copy("firmware.map", map_file)
+    if os.path.isfile(source_map):
+        print(f"Found linker mapfile {source_map}")
+        shutil.copy(source_map, map_file)
+
+env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", bin_rename_copy)
